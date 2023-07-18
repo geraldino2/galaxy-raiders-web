d6ca46bbf21d8b62286a4883b1c55c5ca5221eca
Gabriel Geraldino <gabrielgeraldinosouza@gmail.com>
2023-07-18 09:13:10 -0300

    style the buttons

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,14 @@
 <template>
-  <div id="menu">
-    <button @click='setCookie("gameStarted", "true");showSpaceField=true;'>Start</button>
-    <button @click='setCookie("gameStarted", "exit");exit();'>Exit</button>
-    <button @click='alert("todo");'>Leaderboard</button>
-  </div>
-
   <div id="canvas">
     <div id="deep-space" />
+    <div id="menu">
+      <button class="menu-button" @click='setCookie("gameStarted", "true");showSpaceField=true;'>Start</button>
+      <button class="menu-button" @click='setCookie("gameStarted", "exit");exit();'>Exit</button>
+      <button class="menu-button" @click='alert("todo");'>Update leaderboard</button>
+    </div>
+    <div id="leaderboard">
+      <!--await $get("/leaderboard");-->
+    </div>
     <div id="space-field" v-show="showSpaceField" >
       <SpaceObject id="spaceship" class="spaceship" :data="spaceField.ship" resolution="2" />
 
@@ -40,12 +42,14 @@ function getCookie(name) {
   }
   return unescape(cookie.substring(begin + prefix.length, end));
 }
+
 function setCookie(name, value) {
   let date = new Date();
   date.setTime(date.getTime() + 2592000);
   let expires = "; expires=" + date.toUTCString();
   document.cookie = name + "=" + (value || "")  + expires + "; path=/";
 }
+
 function exit() {
   $get("../exit");
 }
@@ -99,6 +103,29 @@ onMounted(() => {
 </script>
 
 <style>
+#menu {
+  position: relative;
+}
+
+.menu-button {
+  cursor: pointer;
+  border-radius: 100rem;
+  font-size: 1rem;
+  padding: .5rem 3rem;
+  color: $color-black;
+  border: solid 3px transparent;
+  background-image: linear-gradient(rgba(255, 255, 255, 0), rgba(255, 255, 255, 0)), linear-gradient(101deg, #075569, #0f323b);
+  background-origin: border-box;
+  background-clip: content-box, border-box;
+  box-shadow: 2px 1000px 1px #000 inset;
+  color: white;
+}
+
+.menu-button:hover {
+  box-shadow: none;
+  color: black;
+}
+
 #canvas {
   height: calc(100vh - 4rem);
   width: calc(100vw - 4rem);
